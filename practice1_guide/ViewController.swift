9719f8e20620279f936825e334847140dc14e7ab
Fernando Gabriel Martinez <gabrielmtzg76@gmail.com>
2025-06-02 14:39:30 -0600

    Initial Commit

---
+++ practice1_guide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice1_guide
+//
+//  Created by Fernando Gabriel Martinez on 02/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
